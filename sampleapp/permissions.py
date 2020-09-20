f1dcfcbd23c2e987cd61b6db8aaabebda9ce83f2
Fang-Pen <hello@fangpenlin.com>
2020-09-20 15:13:59 -0700

    Remove unused stuff

--- sampleapp/permissions.py
+++ sampleapp/permissions.py
@@ -1,6 +1,4 @@
-from flask_principal import identity_loaded
 from flask_principal import Permission
-from flask_principal import Principal
 from flask_principal import RoleNeed
 
 admin_role_need = RoleNeed("admin")
