2d8d9113208dc567fbd7596d459abedf615190f7
Fang-Pen <hello@fangpenlin.com>
2020-09-20 15:13:38 -0700

    Fix broken tests

--- tests/functional/test_admin.py
+++ tests/functional/test_admin.py
@@ -18,12 +18,15 @@ def test_endpoints_without_user(endpoint, testapp, user):
 
 def test_endpoints_with_not_admin_user(endpoint, testapp, user):
     with testapp.session_transaction() as session:
-        session["user_id"] = user.id
+        session["_user_id"] = user.id
+        session["identity.auth_type"] = None
+        session["identity.id"] = user.id
     testapp.get(url_for(endpoint), status=302)
 
 
 def test_endpoints_with_admin_user(endpoint, testapp, admin_user):
-    assert admin_user.is_admin
     with testapp.session_transaction() as session:
-        session["user_id"] = admin_user.id
+        session["_user_id"] = admin_user.id
+        session["identity.auth_type"] = None
+        session["identity.id"] = admin_user.id
     testapp.get(url_for(endpoint), status=200)
